9713b64668cc80f4f17d241652a743dda145f29a
Andy Stechishin <andy.stechishin@gmail.com>
2016-06-14 10:18:43 -0600

    Need secret key for Heroku
    Add the secret key for environments

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -14,51 +14,45 @@ class ToolsController < ApplicationController
 
   # GET /tools/new
   def new
-    @tool = Tool.new
+    # @tool = Tool.new
+    redirect_to :index
   end
 
   # GET /tools/1/edit
   def edit
+    redirect_to :index
   end
 
   # POST /tools
   # POST /tools.json
   def create
-    @tool = Tool.new(tool_params)
-
-    respond_to do |format|
-      if @tool.save
-        format.html { redirect_to @tool, notice: 'Tool was successfully created.' }
-        format.json { render :show, status: :created, location: @tool }
-      else
-        format.html { render :new }
-        format.json { render json: @tool.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to :index
+    # @tool = Tool.new(tool_params)
+    #
+    # respond_to do |format|
+    #   if @tool.save
+    #     format.html { redirect_to @tool, notice: 'Tool was successfully created.' }
+    #     format.json { render :show, status: :created, location: @tool }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @tool.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /tools/1
   # PATCH/PUT /tools/1.json
   def update
-    respond_to do |format|
-      if @tool.update(tool_params)
-        format.html { redirect_to @tool, notice: 'Tool was successfully updated.' }
-        format.json { render :show, status: :ok, location: @tool }
-      else
-        format.html { render :edit }
-        format.json { render json: @tool.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /tools/1
-  # DELETE /tools/1.json
-  def destroy
-    @tool.destroy
-    respond_to do |format|
-      format.html { redirect_to tools_url, notice: 'Tool was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to :index
+    # respond_to do |format|
+    #   if @tool.update(tool_params)
+    #     format.html { redirect_to @tool, notice: 'Tool was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @tool }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @tool.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   private
