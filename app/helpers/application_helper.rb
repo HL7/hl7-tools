74bb6c2ecfb7dcccff08b61a8c2fd883b5340297
Andy Stechishin <andy.stechishin@gmail.com>
2016-08-29 23:45:30 -0600

    Fixes #1, Fixes #2 Update tool title display
    Change the index and show views to limit the tool title display to word under 50 on the index and words under 30 on the show, add icons for the Internal check and the core process, update table header on index to display icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,37 @@ module ApplicationHelper
     ENV['HL7_TOOL_EDITING'] == 'TRUE'
   end
 
-  def truncate_words(text, length = 30, end_string = '')
-    words = text.split()
-    words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
+  def truncate_words(text, length=50, trailing=nil)
+    if text.length < length
+      text
+    else
+      full_words = ''
+      text.split(/\s+/).each do |word|
+        if (full_words.length + word.length/2) < length
+          full_words << ' ' << word
+        end
+      end
+      if trailing
+        full_words << trailing
+      end
+      full_words
+    end
+  end
+
+  def reformat_para(s, width=80)
+    lines = []
+    line = ''
+    s.split(/\s+/).each do |word|
+      if line.size + word.size >= width
+        lines << line
+        line = word
+      elsif line.empty?
+        line = word
+      else
+        line << ' ' << word
+      end
+    end
+    lines << line if line
+    lines
   end
 end
